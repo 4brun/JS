6ec804b12763de0a03487ecf37aba8d375d898ee
Dmitriy <dabrun@gmail.com>
2021-05-18 19:43:14 +0300

    add game

---
+++ lession-4/gameCode.js
@@ -0,0 +1,137 @@
+//После игры необходимо спросить номер вопроса. 
+//По номеру вопроса нужно вывести текст вопроса и текст выбранного ответа
+
+var event, ok;
+
+var answers = [];
+
+do {//Выводим первый вопрос
+    ok = false;
+    event = +prompt(works.a00 + works.a1 + works.a2 + '-1 - Выход из игры');
+    answers.push(works.a00);
+    if (event == -1) {
+        break;
+    }
+    else {
+        ok = isAnswer(works.a0, event);
+    }
+} while (!ok);
+switch (event) {
+    case 1: // Первое действие  - если в первом окне ввели 1 то открываем серию окон - окно 2
+        do {
+            ok = false;
+            event = +prompt(works.b00 + works.b1 + works.b2 + '-1 - Выход из игры');
+            answers.push(works.b00);
+            if (event == -1) {
+                break;
+            }
+            else {
+                ok = isAnswer(works.b0, event);
+            }
+        } while (!ok);
+        switch (event) {
+            case 1: // Второе действие, если во 2 окне ввели 1 то переходим на 4 окно
+                do {
+                    ok = false;
+                    event = +prompt(works.d00 + works.d1 + works.d2 + '-1 - Выход из игры');
+                    answers.push(works.d00);
+                    if (event == -1) {
+                        break;
+                    }
+                    else {
+                        ok = isAnswer(works.d0, event);
+                    }
+                } while (!ok);
+
+                break;
+            case 2: // Второе действие   Если ввели 2 то также переходим на 4 окно
+                do {
+                    ok = false;
+                    event = +prompt(works.d00 + works.d1 + works.d2 + '-1 - Выход из игры');
+                    answers.push(works.d00);
+                    if (event == -1) {
+                        break;
+                    }
+                    else {
+                        ok = isAnswer(works.d0, event);
+                    }
+                } while (!ok);
+
+                break;
+            case -1: // Второе действие
+                break;
+            default:
+                alert('Ошибка');
+        }
+        break;
+    case 2: // Первое действие    Если в 1 окне ввели 2 то переходим к 3 окну
+        do {
+            ok = false;
+            event = +prompt(works.c00 + works.c1 + works.c2 + '-1 - Выход из игры');
+            answers.push(works.c00);
+            if (event == -1) {
+                break;
+            }
+            else {
+                ok = isAnswer(works.c0, event);
+            }
+        } while (!ok);
+        switch (event) {
+            case 1: // Второе действие
+                do {
+                    ok = false;
+                    event = +prompt(works.d00 + works.d1 + works.d2 + '-1 - Выход из игры');
+                    answers.push(works.d00);
+                    if (event == -1) {
+                        break;
+                    }
+                    else {
+                        ok = isAnswer(works.d0, event);
+                    }
+                } while (!ok);
+
+                break;
+            case 2: // Второе действие
+                do {
+                    ok = false;
+                    event = +prompt(works.d00 + works.d1 + works.d2 + '-1 - Выход из игры');
+                    answers.push(works.d00);
+                    if (event == -1) {
+                        break;
+                    }
+                    else {
+                        ok = isAnswer(works.d0, event);
+                    }
+                } while (!ok);
+
+                break;
+            case -1: // Второе действие
+                break;
+            default:
+                alert('Ошибка');
+        }
+        break;
+    case -1: // Первое действие
+        break;
+    default:
+        alert('Ошибка');
+}
+//alert('Спасибо за игру');
+var finalAnswer = +prompt('Спасибо за игру, \nВведите номер хода, чтобы узнать ответ: ');
+
+alert("Ваш ответ на вопрос №" + finalAnswer + "\n" + answers[finalAnswer - 1]);
+
+//------------------------------------------
+function isAnswer(q, event) {
+    if (isNaN(event) || !isFinite(event)) {
+        alert('Вы ввели недопустимый символ');
+        return false;
+    }
+    else if (event < 1 || event > q) {
+        alert('Ваше число выходит из допустимого диапозона');
+        return false;
+    }
+    return true;
+
+}
+
